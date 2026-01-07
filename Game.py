25306b2a05f1c4078689b4dce914dd008797baf9
Seweryn <lipski.seweryn@o2.pl>
2026-01-07 18:13:59 +0100

    commit

--- Game.py
+++ Game.py
@@ -220,7 +220,7 @@ class Game:
         self.victory = True
 
     def spawn_enemies_randomly(self, grid_x: int, grid_y: int, pos: tuple[int, int]) -> None:
-        if random.randint(0, 400) < 10:
+        if random.randint(0, 400) < 8:
             dist_x = abs(grid_x * TILE_SIZE - WIDTH / 2)
             dist_y = abs(grid_y * TILE_SIZE - HEIGHT / 2)
 
